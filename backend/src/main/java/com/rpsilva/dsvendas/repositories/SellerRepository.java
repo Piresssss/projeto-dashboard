36c35d15768e84ee5b430570f8628419f4f0b22d
Piresssss <faelpiressilva@hotmail.com>
2021-09-10 21:27:59 -0300

    Layers

---
+++ backend/src/main/java/com/rpsilva/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.rpsilva.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rpsilva.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
